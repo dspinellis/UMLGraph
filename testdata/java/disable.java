39048e6f0f99c471d8bfb5a50267e728009b0630
Andrea Aime <andrea.aime@gmail.com>
2005-12-28 14:57:22 +0000

    New test to check local option disabling

---
+++ testdata/java/disable.java
@@ -0,0 +1,40 @@
+// $Id$
+
+/**
+ * Locally disable an option (test)
+ *
+ * @opt operations 
+ * @opt attributes 
+ * @opt types 
+ * @opt visibility 
+ * @hidden
+ */
+class UMLOptions {}
+
+/**
+ * @opt !operations
+ */
+class Person1 {
+    String address;
+    String name;
+    String displayName();
+}
+
+/**
+ * @opt !attributes
+ */
+class Person2 {
+    Address address;
+    String name;
+    String displayName();
+}
+
+/**
+ * @opt !visibility
+ */
+class Person3 {
+    Address address;
+    String name;
+    String displayName();
+}
+
