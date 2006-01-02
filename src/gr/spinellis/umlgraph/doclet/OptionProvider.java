b77d5a9eb6d22ce2b38a863e26af2065aae4c2b9
Andrea Aime <andrea.aime@gmail.com>
2006-01-02 14:03:52 +0000

    Added javadoc comments Added a couple of methods to manage view inheritance

--- src/gr/spinellis/umlgraph/doclet/OptionProvider.java
+++ src/gr/spinellis/umlgraph/doclet/OptionProvider.java
@@ -2,10 +2,33 @@ package gr.spinellis.umlgraph.doclet;
 
 import com.sun.javadoc.ClassDoc;
 
+/**
+ * A factory class that builds Options object for general use or for a
+ * specific class
+ */
 public interface OptionProvider {
+    /**
+     * Returns the options for the specified class. 
+     */
     public Options getOptionsFor(ClassDoc cd);
     
+    /**
+     * Returns the options for the specified class. 
+     */
     public Options getOptionsFor(String name);
     
+    /**
+     * Returns the global options (the class independent definition) 
+     */
     public Options getGlobalOptions();
+    
+    /**
+     * Gets a base Options and applies the overrides for the specified class 
+     */
+    public void overrideForClass(Options opt, ClassDoc cd);
+    
+    /**
+     * Gets a base Options and applies the overrides for the specified class 
+     */
+    public void overrideForClass(Options opt, String className);
 }
