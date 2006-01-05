fe09e407d1c00ce1215674d9536515965807209e
Andrea Aime <andrea.aime@gmail.com>
2006-01-05 11:04:02 +0000

    Relationship inference documentation

---
+++ doc/infer.java
@@ -0,0 +1,38 @@
+/*
+ * Relationship inference
+ * $Id$
+ */
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @opt inferrel
+ * @opt collpackages java.util.*
+ * @opt inferdep
+ * @opt hide java.*
+ * @hidden
+ */
+class UMLOptions {}
+
+class Person {
+    House[] houses;
+    List<Dog> dogs;
+    
+    public Room getFavouriteRoom(House house) {}
+}
+
+/**
+ * @opt inferreltype composed
+ */
+class House {
+    Map<String, Room> nameRoomMap;
+}
+
+class Room {}
+
+class Dog {
+    Person owner;
+}
