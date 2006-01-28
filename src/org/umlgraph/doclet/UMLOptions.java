7edade65bb0619874212e5af587082df85dda459
Andrea Aime <andrea.aime@gmail.com>
2006-01-28 10:14:17 +0000

    Changed the class diagram generation options, with the new classes the old diagram was getting hard to understand at a glance

--- src/org/umlgraph/doclet/UMLOptions.java
+++ src/org/umlgraph/doclet/UMLOptions.java
@@ -3,8 +3,6 @@ package gr.spinellis.umlgraph.doclet;
 /**
  * Options for UMLGraph class diagram generation
  * @opt operations
- * @opt constructors
- * @opt qualify
  * @opt visibility
  * @hidden
  */
