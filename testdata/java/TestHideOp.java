f7f0725667a98b7d971c4aec6cd5a67e9658ab63
Andrea Aime <andrea.aime@gmail.com>
2006-02-23 19:46:45 +0000

    New regression test to make sure the fixed bug doesn't get back

---
+++ testdata/java/TestHideOp.java
@@ -0,0 +1,15 @@
+// $Id$
+package test;
+
+/**
+ * @hidden
+ * @opt operations
+ */
+class UMLOptions{}
+
+public class TestHideOp {
+    /**
+     * @hidden
+     */
+    public void hiddenOperation();
+}
