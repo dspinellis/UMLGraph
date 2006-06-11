015357b8c70804cf8c1c520cacc7562042f20f6f
Andrea Aime <andrea.aime@gmail.com>
2006-06-11 13:42:00 +0000

    Java visibility types enumeration, with some useful conversion methods for actually making it easy to use in the context of UmlGraph

---
+++ src/org/umlgraph/doclet/Visibility.java
@@ -0,0 +1,58 @@
+/*
+ * Create a graphviz graph based on the classes in the specified java
+ * source files.
+ *
+ * (C) Copyright 2002-2005 Diomidis Spinellis
+ *
+ * Permission to use, copy, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $Id$
+ *
+ */
+package gr.spinellis.umlgraph.doclet;
+
+import com.sun.javadoc.ProgramElementDoc;
+
+/**
+ * Enumerates the possible visibilities in a Java program. For brevity, package
+ * private visibility is referred as PACKAGE.
+ * @author wolf
+ * 
+ */
+public enum Visibility {
+    PRIVATE("private"), PACKAGE("package"), PROTECTED("protected"), PUBLIC("public");
+
+    private String name;
+
+    private Visibility(String name) {
+	this.name = name;
+    }
+
+    public static Visibility get(ProgramElementDoc doc) {
+	if (doc.isPrivate())
+	    return PRIVATE;
+	else if (doc.isPackagePrivate())
+	    return PACKAGE;
+	else if (doc.isProtected())
+	    return PROTECTED;
+	else
+	    return PUBLIC;
+
+    }
+
+    public static Visibility parseVisibility(String vis) {
+	for (Visibility v : Visibility.values()) {
+	    if (v.name.equalsIgnoreCase(vis))
+		return v;
+	}
+	return null;
+    }
+}
