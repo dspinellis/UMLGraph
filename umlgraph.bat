84b14f46e74d3c7d061acf75059994b8eceb01f1
Diomidis Spinellis <dds@aueb.gr>
2007-05-06 14:29:33 +0000

    Package visibility is best for generating diagrams.

--- umlgraph.bat
+++ umlgraph.bat
@@ -9,7 +9,7 @@ echo usage: umlgraph base_file_name filetype [umlgraph arguments]
 goto end
 
 :ok
-javadoc -docletpath lib/UmlGraph.jar -doclet gr.spinellis.umlgraph.doclet.UmlGraph -private %3 %4 %5 %6 %7 %8 %9 -output %1.dot %1.java
+javadoc -docletpath lib/UmlGraph.jar -package -doclet gr.spinellis.umlgraph.doclet.UmlGraph %3 %4 %5 %6 %7 %8 %9 -output %1.dot %1.java
 dot -T%2 -o%1.%2 %1.dot
 
 :end
