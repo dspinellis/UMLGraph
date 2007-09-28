ca0b8c59c965cbf5cefccf0e8548122d141142cb
Diomidis Spinellis <dds@aueb.gr>
2007-09-28 11:32:21 +0000

    Drop the requirement for copying Tools.jar. This follows a suggestion by Vadim Nasardinov.

--- umlgraph.bat
+++ umlgraph.bat
@@ -1,11 +1,12 @@
 @echo off
 
 rem Windows batch file to run UMLGraph on the specified base file name
-rem For this to work the JDK tools.jar file must be located in the
-rem same directory as UmlGraph.jar.  You also need to adjust the path
-rem of UmlGraph.jar to the actual path where the jar file is installed.
+rem For this to work you must adjust the following defintion of
+rem UMLGRAPH_HOME to point to the directory where UmlGraph.jar is installed.
 
 rem $Id$
+
+set UMLGRAPH_HOME=lib
 
 if not "%2" == "" goto ok
 echo usage: umlgraph base_file_name filetype [umlgraph arguments] 1>&2
@@ -14,6 +15,7 @@ echo (The above will convert MyClass.java into MyClass.png) 1>&2
 goto end
 
 :ok
-java -jar lib/UmlGraph.jar -package %3 %4 %5 %6 %7 %8 %9 -output - %1.java | dot -T%2 -o%1.%2
+
+java -classpath "%UMLGRAPH_HOME%\UmlGraph.jar;%JAVA_HOME%\lib\Tools.jar" gr.spinellis.umlgraph.doclet.UmlGraph -package %3 %4 %5 %6 %7 %8 %9 -output - %1.java | dot -T%2 -o%1.%2
 
 :end
