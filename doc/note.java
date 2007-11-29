f602edc5873381755f5e43c9e09a0b2ab1a48a0a
Diomidis Spinellis <dds@aueb.gr>
2007-11-29 13:02:58 +0000

    Document new functionality.

---
+++ doc/note.java
@@ -0,0 +1,26 @@
+/**
+ * @opt shape node
+ * @note Located in the
+ * machine room
+ * @note Sun Blade 6048
+ * @depend - - - MapLocation
+ * @depend - - - DataMine
+ */
+class Server{}
+
+/** @opt shape component */
+class MapLocation {}
+
+/** @opt shape component */
+class DataMine {}
+
+/**
+ * CPU-munching
+ * components that must
+ * run on this server
+ * @opt shape note
+ * @opt commentname
+ * @assoc - - - MapLocation
+ * @assoc - - - DataMine
+ */
+class munchComment {}
