a54b9bfc40080b28d4228e743ebf84792d628f5c
Diomidis Spinellis <dds@aueb.gr>
2013-05-23 19:44:52 +0300

    For Maven to work Version.java must exist
    New Version.java can be created with "ant version"

--- src/main/java/org/umlgraph/doclet/Version.java
+++ src/main/java/org/umlgraph/doclet/Version.java
@@ -1,29 +1,4 @@
+/* Automatically generated file */
 package org.umlgraph.doclet;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-class Version {
-    private final static String filename = "version.properties";
-    private final static String key = "umlgraph.version";
-    public static String VERSION;
-
-    static {
-        Properties props = new Properties();
-        InputStream in = Version.class.getClassLoader().getResourceAsStream(filename);
-        try {
-            props.load(in);
-            if (props.get(key) == null) {
-                System.err.println("Could not find version property");
-                VERSION = "0";
-            } else {
-                VERSION = props.get(key).toString();
-            }
-
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+class Version { public static String VERSION = "R5_6-24-gf6e263";}
+	
