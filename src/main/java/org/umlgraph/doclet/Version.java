c4f32a5a741c5b2dd49465be8b6c725d37104f36
Diomidis Spinellis <dds@aueb.gr>
2016-06-07 17:20:48 +0300

    Update test reference files

--- src/main/java/org/umlgraph/doclet/Version.java
+++ src/main/java/org/umlgraph/doclet/Version.java
@@ -1,4 +1,4 @@
 /* Automatically generated file */
 package org.umlgraph.doclet;
-class Version { public static String VERSION = "R5_7_2-3-gee82a7";}
+class Version { public static String VERSION = "R5_7_2-11-g86574a";}
 	
