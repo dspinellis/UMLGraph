742ee6f73ed9ce8915e2365acc2c31de60ce640d
Fotis Xenakis <foxen@windowslive.com>
2019-04-09 19:07:53 +0300

    Remove unnecessary super() call (extends Object)

--- src/main/java/org/umlgraph/doclet/PackageMatcher.java
+++ src/main/java/org/umlgraph/doclet/PackageMatcher.java
@@ -7,7 +7,6 @@ public class PackageMatcher implements ClassMatcher {
     protected PackageDoc packageDoc;
 
     public PackageMatcher(PackageDoc packageDoc) {
-	super();
 	this.packageDoc = packageDoc;
     }
 
